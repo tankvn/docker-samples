03b6823cb9f77c38f573e59b627a60e67e59572c
Tank.vn <tanvd357@gmail.com>
2022-07-30 16:10:29 +0700

    Connect to a MongoDB Database Using Node.js

---
+++ nodejs-mongodb/README.md
@@ -0,0 +1,27 @@
+## Simple Docker samples
+### NodeJS application with a NodeJS backend and a MongoDB database
+
+#### Step 1 - Installing Mongoose on a Node.js environment
+
+Create and navigate to a new folder by running the following commands on a terminal.
+```bat
+mkdir nodejs-mongodb
+cd nodejs-mongodb
+```
+
+Instantly initialize a project
+```bat
+npm init --yes
+```
+
+#### Step 2 – Creating the connection
+
+Then connect to a local `MongoDB` instance using the `MongoClient` function.
+```javascript
+MongoClient.connect('mongodb://localhost:27017/testdb');
+```
+
+#### Step 3 – Run or Execute JavaScript File in Node.js
+Now, run your web server using `node index.js`.
+
+Visit http://127.0.0.1:3000 and you will see a message saying "Hello World".
--- nodejs-mongodb/index.js
+++ nodejs-mongodb/index.js
@@ -1,8 +1,32 @@
 // Importing http core package/module provided by nodejs
 const http = require('http');
 
+// Lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+// We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
 const hostname = '127.0.0.1';
 const port = 3000;
+
+// Create a database named "testdb":
+var url = "mongodb://localhost:27017/testdb";
+
+MongoClient.connect(url, function(err, db) {
+	if (err) {
+		console.log('Unable to connect to the mongoDB server. Error:', err);
+	} else {
+		//HURRAY!! We are connected. :)
+		console.log('Connection established to', url);
+
+		// do some work here with the database.
+		console.log("Database created!");
+
+		//Close connection
+		db.close();
+	}
+});
 
 // Creating a server
 const server = http.createServer((req, res) => {
